d7d8d8438b279ff839aec5789e2b96714375e0f0
Francesco T <0x1337@protonmail.ch>
2020-05-01 20:23:34 +0200

    Final Test 1

--- progetto/js/main.js
+++ progetto/js/main.js
@@ -4,17 +4,26 @@
  * Se impostato su zero o su un valore al di fuori della dimensione di argList
  * nessuno dei bottoni verra' stilizato.
 */
-var argList = ["Arg1","Arg2","Arg3","Arg4","Arg5"];
+var argList = ["Home Page","Poèmes d’amour","Poèmes sur le temps qui passe","Poèmes sur la nature"];
 
 function loadMenu(active = 0) {
 	var div = document.createElement("div");
 	div.setAttribute("id","menu-bar");
-	var a, content;
-	for(var i = 1; i <= argList.length; i++) {
+	var a = document.createElement("a");
+	var content = document.createTextNode(argList[0]);
+	a.appendChild(content);
+	a.href = "./index.html";
+	if(active == 1) {
+		a.classList = "element select";
+	} else {
+		a.classList = "element";
+	}
+	div.appendChild(a);
+	for(var i = 2; i <= argList.length; i++) {
 		a = document.createElement("a");
 		content = document.createTextNode(argList[i-1]);
 		a.appendChild(content);
-		a.href = "./page" + i + ".html";
+		a.href = "./page" + (i-1) + ".html";
 		if(i == active) {
 			a.classList = "element select";
 		} else {
